01b4af70763690873e4459ccc96f899a26c0fb72
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-02-07 18:51:25 +0100

    made a basic version of the filter method

---
+++ lesson1/filterEmulation.js
@@ -0,0 +1,16 @@
+function filter(array, callback) {
+  const results = [];
+  for (let i = 0; i < array.length; i++) {
+    if (callback(array[i])) results.push(array[i]);
+  }
+  return results;
+}
+
+let numbers = [1, 2, 3, 4, 5];
+console.log(filter(numbers, (number) => number > 3)); // => [ 4, 5 ]
+console.log(filter(numbers, (number) => number < 0)); // => []
+console.log(filter(numbers, () => true)); // => [ 1, 2, 3, 4, 5 ]
+
+let values = [1, "abc", null, true, undefined, "xyz"];
+console.log(filter(values, (value) => typeof value === "string"));
+// => [ 'abc', 'xyz' ]
