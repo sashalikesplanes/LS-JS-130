dff904a7d8e4942430eadf0dcdedea74167ff7d4
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-02-07 18:38:43 +0100

    added ability to specify context

--- lesson1/forEachRemake.js
+++ lesson1/forEachRemake.js
@@ -1,7 +1,7 @@
-function forEach(arr, callbackfn) {
+function forEach(arr, callbackfn, context) {
   const length = arr.length;
   for (let i = 0; i < length; i++) {
-    callbackfn(arr[i]);
+    callbackfn.call(context, arr[i]);
   }
 }
 
@@ -12,3 +12,21 @@ arr.forEach((value) => console.log(value * value));
 
 // Our forEach function
 forEach(arr, (value) => console.log(value * value));
+
+class Foo {
+  constructor(prefix) {
+    this.prefix = prefix;
+  }
+
+  showItem(item) {
+    console.log(this.prefix, item);
+  }
+}
+
+let foo = new Foo("Item: ");
+// [1, 2, 3].forEach(foo.showItem, foo);
+// [4, 5, 6].forEach(foo.showItem);
+
+forEach(["a", "b", "c"], (item) => console.log(item));
+forEach([1, 2, 3], foo.showItem, foo);
+// forEach([4, 5, 6], foo.showItem);
