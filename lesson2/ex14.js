8b6d4376d84115224d09d9f8f4e8ba8a4e827cf3
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-02-08 21:43:26 +0100

    solved ex14

---
+++ lesson2/ex14.js
@@ -0,0 +1,11 @@
+let bar = ((start) => {
+  let prod = start;
+  return function (factor) {
+    prod *= factor;
+    return prod;
+  };
+})(2);
+let result = bar(3);
+result += bar(4);
+result += bar(5);
+console.log(result);
