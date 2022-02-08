42410814359a2b5c7f139f5a6f733ca6d2dd3bee
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-02-08 23:26:20 +0100

    solved exercise

---
+++ lesson2/shortHand4.js
@@ -0,0 +1,13 @@
+function foo(array) {
+  let one = array[0];
+  let three = array[2];
+  return [three, 5, one];
+}
+
+let array = [1, 2, 3];
+let result = foo(array);
+[bar, qux, baz];
+
+let bar = array[0];
+let qux = array[1];
+let baz = array[2];
