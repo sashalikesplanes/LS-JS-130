2e01260f35f9ea080e8fa771b34e4e0dbf4a055d
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-02-09 15:56:40 +0100

    solved exercise;

---
+++ exercises/misc/ex2.js
@@ -0,0 +1,7 @@
+function formatName(firstName, middleName, lastName) {
+  return `${lastName}, ${firstName} ${middleName[0]}.`;
+}
+
+fullName = ["James", "Tiberius", "Kirk"];
+
+console.log(formatName(...fullName));
