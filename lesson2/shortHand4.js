7fd861bbce116903e2c6c260431a5b5e86bdc557
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-02-09 13:47:49 +0100

    minor updates to shorthand exercise to make the solution better

--- lesson2/shortHand4.js
+++ lesson2/shortHand4.js
@@ -8,6 +8,6 @@ let array = [1, 2, 3];
 let result = foo(array);
 [bar, qux, baz];
 
-let bar = array[0];
-let qux = array[1];
-let baz = array[2];
+let bar = result[0];
+let qux = result[1];
+let baz = result[2];
