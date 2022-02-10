1e1433a773ad4ffc1ad8992081480d18142ca56f
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-02-10 22:27:29 +0100

    played around more with tests

--- lesson3/jest_testing/car.js
+++ lesson3/jest_testing/car.js
@@ -1,10 +1,15 @@
 class Car {
   constructor() {
     this.wheels = 4;
+    this.mileageInfo = null;
   }
 
   canFly() {
     return true;
+  }
+
+  fly() {
+    throw Error("cannot fly yet");
   }
 }
 
--- lesson3/jest_testing/car.test.js
+++ lesson3/jest_testing/car.test.js
@@ -2,6 +2,7 @@ const Car = require("./car");
 
 describe("The Car class", () => {
   let car = new Car();
+  let car2 = new Car();
   test("has four wheels", () => {
     expect(car.wheels).toBe(4);
   });
@@ -12,5 +13,31 @@ describe("The Car class", () => {
 
   xtest("bad wheels", () => {
     expect(car.wheels).toBe(3);
+  });
+
+  test("object equality", () => {
+    expect({ a: 1, b: 2 }).toEqual({ a: 1, b: 2 });
+  });
+
+  test("item equality with toEqual", () => {
+    expect("1").toEqual("1");
+  });
+
+  test("car is equal to car2", () => {
+    expect(car).toEqual(car2);
+  });
+
+  test("raises an error when undefined method is called", () => {
+    // needs to be wrapped in a function
+    expect(() => car.drive()).toThrow();
+  });
+
+  xtest("function passed to expect is automatically called", () => {
+    // does not automatically call the function but checks if it throws?
+    expect(() => 2).toBe(2);
+  });
+
+  test("a method which throws an exception", () => {
+    expect(() => car.fly()).toThrow();
   });
 });
