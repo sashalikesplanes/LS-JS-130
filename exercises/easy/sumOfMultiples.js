ef6cefbb673c8ba9949409dfbe0e9cbd74205f06
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-02-13 14:56:22 +0100

    solved exercise

--- exercises/easy/sumOfMultiples.js
+++ exercises/easy/sumOfMultiples.js
@@ -1,3 +1,36 @@
-class SumOfMultiples {}
+class SumOfMultiples {
+  constructor(...baseMultiples) {
+    this.baseMultiples = baseMultiples;
+  }
+
+  to(target) {
+    return SumOfMultiples.to(target, this.baseMultiples);
+  }
+
+  static to(target, baseMultiples = [3, 5]) {
+    const allMultiples = this.getAllMultiples(target, baseMultiples);
+    return allMultiples.reduce((sum, current) => sum + current, 0); // 0 needed in case empty
+    // iterate from smallest multiple to < target
+    // if any of numbers divisible by base, add it to list
+    // sum the list
+  }
+
+  static getAllMultiples(target, baseMultiples) {
+    const multiples = [];
+    baseMultiples.forEach((baseMultiple) => {
+      let currentMultiple = baseMultiple;
+      while (currentMultiple < target) {
+        if (!multiples.includes(currentMultiple)) {
+          multiples.push(currentMultiple);
+        }
+        currentMultiple += baseMultiple;
+      }
+    });
+    return multiples;
+  }
+}
 
 module.exports = SumOfMultiples;
+
+let obj = new SumOfMultiples(7, 13, 17);
+console.log(obj.to(20));
--- exercises/easy/sumOfMultiples.test.js
+++ exercises/easy/sumOfMultiples.test.js
@@ -5,38 +5,38 @@ describe("sum of multiples", () => {
     expect(SumOfMultiples.to(1)).toEqual(0);
   });
 
-  xtest("sum to three", () => {
+  test("sum to three", () => {
     expect(SumOfMultiples.to(4)).toEqual(3);
   });
 
-  xtest("sum to ten", () => {
+  test("sum to ten", () => {
     expect(SumOfMultiples.to(10)).toEqual(23);
   });
 
-  xtest("sum to one hundred", () => {
+  test("sum to one hundred", () => {
     expect(SumOfMultiples.to(100)).toEqual(2_318);
   });
 
-  xtest("sum to one thousand", () => {
+  test("sum to one thousand", () => {
     expect(SumOfMultiples.to(1000)).toEqual(233_168);
   });
 
-  xtest("configurable 7 13 17 to 20", () => {
+  test("configurable 7 13 17 to 20", () => {
     let obj = new SumOfMultiples(7, 13, 17);
     expect(obj.to(20)).toEqual(51);
   });
 
-  xtest("configurable 4 6 to 15", () => {
+  test("configurable 4 6 to 15", () => {
     let obj = new SumOfMultiples(4, 6);
     expect(obj.to(15)).toEqual(30);
   });
 
-  xtest("configurable 5 6 8 to 150", () => {
+  test("configurable 5 6 8 to 150", () => {
     let obj = new SumOfMultiples(5, 6, 8);
     expect(obj.to(150)).toEqual(4_419);
   });
 
-  xtest("configurable 43 47 to 10,000", () => {
+  test("configurable 43 47 to 10,000", () => {
     let obj = new SumOfMultiples(43, 47);
     expect(obj.to(10_000)).toEqual(2_203_160);
   });
