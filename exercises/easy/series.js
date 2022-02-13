f8577bfc1f949d8e13b58007cfb64fe93fafbefa
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-02-13 16:48:45 +0100

    solved exefcisde

--- exercises/easy/series.js
+++ exercises/easy/series.js
@@ -7,6 +7,13 @@ class Series {
       throw Error("slice length longer than sequence");
     }
     const listOfSlices = [];
+    // Iterate over the array until certain end point
+    // add to list of slices a slice from current index to desired length + index
+    for (let index = 0; index <= this.numArr.length - length; index++) {
+      listOfSlices.push(this.numArr.slice(index, index + length));
+    }
     return listOfSlices;
   }
 }
+
+module.exports = Series;
