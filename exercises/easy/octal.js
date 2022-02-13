0e544e3e6507f4f9cc8a6aee0adfef95f4f6a4b0
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-02-13 16:40:04 +0100

    solved exercise

--- exercises/easy/octal.js
+++ exercises/easy/octal.js
@@ -1,3 +1,19 @@
-class Octal {}
+class Octal {
+  constructor(octalString) {
+    this.octalString = octalString;
+  }
+
+  toDecimal() {
+    if (!Octal._validOctalString(this.octalString)) return 0;
+    let decimalNum = 0;
+    const reverse = this.octalString.split("").reverse();
+    reverse.forEach((num, exp) => (decimalNum += num * 8 ** exp));
+    return decimalNum;
+  }
+
+  static _validOctalString(string) {
+    return string.split("").every((char) => "01234567".includes(char));
+  }
+}
 
 module.exports = Octal;
