7e39641bc35ba74a3a13129ab28a9c018b17a2c8
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-02-14 18:23:31 +0100

    implement robots class

--- exercises/medium/robot-name.js
+++ exercises/medium/robot-name.js
@@ -1,5 +1,7 @@
 class Robot {
   static usedNames = [];
+  static VALID_LETTERS = "ABCDEFGHIJKLMNPQRSTUVWXYZ";
+  static VALID_NUMBERS = "0123456789";
   constructor() {
     this.name = Robot.getUniqueName();
   }
@@ -14,6 +16,26 @@ class Robot {
       uniqueName = Robot.getRandomName();
     }
     return uniqueName;
+  }
+
+  static getRandomName() {
+    return (
+      Robot.getRandomChars(2, Robot.VALID_LETTERS) +
+      Robot.getRandomChars(3, Robot.VALID_NUMBERS)
+    );
+  }
+
+  static getRandomChars(number, validChars) {
+    let chars = "";
+    for (let i = 0; i < number; i++) {
+      chars += this.getRandomChar(validChars);
+    }
+    return chars;
+  }
+
+  static getRandomChar(validChars) {
+    const randomChoice = Math.floor(Math.random() * validChars.length);
+    return validChars[randomChoice];
   }
 }
 
