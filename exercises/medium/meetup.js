4e0e23ca1cd1e44bafb4ad1cea09b68070002302
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-02-14 19:30:11 +0100

    implemented day method

--- exercises/medium/meetup.js
+++ exercises/medium/meetup.js
@@ -5,7 +5,27 @@ class Meetup {
   }
   day(weekday, timing) {
     const meetupDate = new Date();
-    // set to correct year, month and date
+    const weekdayIdx = getWeekdayIdx(weekday);
+    meetupDate.setFullYear(this.year);
+    meetupDate.setMonth(this.month);
+    switch (timing.toLowerCase()) {
+      case "first":
+        return setToNthDay(meetupDate, 1, weekday);
+      case "second":
+        return setToNthDay(meetupDate, 2, weekday);
+      case "third":
+        return setToNthDay(meetupDate, 3, weekday);
+      case "fourth":
+        return setToNthDay(meetupDate, 4, weekday);
+      case "fifth":
+        return setToNthDay(meetupDate, 5, weekday);
+      case "last":
+        return setToLastDay(meetupDate, weekday);
+      case "teenth":
+        return setToTeenthDay(meetupDate, weekday);
+    }
+    // set to correct year, start of month
+
     return meetupDate;
   }
 }
