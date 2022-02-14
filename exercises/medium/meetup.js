fa41891127974a4240cb78f48042a353edc31774
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-02-14 19:24:26 +0100

    spike Meetup

---
+++ exercises/medium/meetup.js
@@ -0,0 +1,11 @@
+class Meetup {
+  constructor(year, monthNum) {
+    this.year = year;
+    this.month = monthNum - 1;
+  }
+  day(weekday, timing) {
+    const meetupDate = new Date();
+    // set to correct year, month and date
+    return meetupDate;
+  }
+}
