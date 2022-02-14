f177df7fb36c18725fe2f2ea44ed74e31f5d6a03
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-02-14 18:33:42 +0100

    spike the CustomSet

---
+++ exercises/medium/custom-set.js
@@ -0,0 +1,31 @@
+class CustomSet {
+  constructor(elements) {}
+  isEmpty() {
+    // return Bool
+  }
+  contains() {
+    // return Bool
+  }
+  isSubset(set) {
+    // return Bool
+  }
+  isDisjoint(set) {
+    // return Bool
+  }
+  isSame(set) {
+    // return Bool
+  }
+  add(element) {
+    // TODO
+    return this;
+  }
+  intersection(set) {
+    // return new CustomSet
+  }
+  difference(set) {
+    // return new CustomSet
+  }
+  union(set) {
+    // return new CustomSet
+  }
+}
