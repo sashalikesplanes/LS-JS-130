341586c8aa431ed10bcaf86b24ae014faa14f388
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-02-14 17:54:40 +0100

    implement top of the diamon

--- exercises/medium/diamond.js
+++ exercises/medium/diamond.js
@@ -1,5 +1,22 @@
 class Diamond {
-  static makeDiamond(letter) {}
-}
+  static makeDiamond(letter) {
+    let result = "";
+    const maxRowIndex = letter.charCodeAt() - "A".charCodeAt();
+    for (let row = 0; row <= maxRowIndex; row++) {
+      result += " ".repeat(maxRowIndex - row);
+      result += Diamond.makeRow(row);
+      result += " ".repeat(maxRowIndex - row) + "\n";
+    }
+    return result;
+  }
 
+  static makeRow(index) {
+    if (index === 0) return "A";
+    else {
+      const currentLetter = String.fromCharCode("A".charCodeAt() + index);
+      return currentLetter + " ".repeat(index * 2 - 1) + currentLetter;
+    }
+  }
+}
+console.log(Diamond.makeDiamond("D"));
 module.exports = Diamond;
