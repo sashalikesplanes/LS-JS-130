15df94f4c7bf3465e3a5cd6f8023d319ca4b7382
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-02-14 17:36:36 +0100

    enable all tests, code is ready

--- exercises/medium/clock.test.js
+++ exercises/medium/clock.test.js
@@ -15,7 +15,7 @@ describe("Clock", () => {
     expect(clock.toString()).toBe("10:03");
   });
 
-  xtest("subtract fifty minutes", () => {
+  test("subtract fifty minutes", () => {
     let clock = Clock.at(0).subtract(50);
     expect(clock.toString()).toBe("23:10");
   });
@@ -35,12 +35,12 @@ describe("Clock", () => {
     expect(clock.toString()).toBe("13:01");
   });
 
-  xtest("subtract a few minutes", () => {
+  test("subtract a few minutes", () => {
     let clock = Clock.at(10, 30).subtract(5);
     expect(clock.toString()).toBe("10:25");
   });
 
-  xtest("subtract minutes", () => {
+  test("subtract minutes", () => {
     let clock = Clock.at(10).subtract(90);
     expect(clock.toString()).toBe("08:30");
   });
@@ -52,12 +52,12 @@ describe("Clock", () => {
     expect(clock1.isEqual(clock2)).toBe(true);
   });
 
-  xtest("wrap around at negative midnight", () => {
+  test("wrap around at negative midnight", () => {
     let clock = Clock.at(0, 30).subtract(60);
     expect(clock.toString()).toBe("23:30");
   });
 
-  xtest("subtract more than a day", () => {
+  test("subtract more than a day", () => {
     let clock = Clock.at(10).subtract(3061);
     expect(clock.toString()).toBe("06:59");
   });
@@ -71,7 +71,7 @@ describe("Clock", () => {
     expect(clock1.isEqual(clock3)).toBe(false);
   });
 
-  xtest("wrap around backwards", () => {
+  test("wrap around backwards", () => {
     let clock = Clock.at(0, 30).subtract(60);
     expect(clock.toString()).toEqual("23:30");
   });
