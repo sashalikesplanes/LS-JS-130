029a6247adaad5f089477dd159bf3d8c852fa663
Alexander Kiselev <a.kiselev@student.tudelft.nl>
2022-02-14 18:16:53 +0100

    add spike of robot class

---
+++ exercises/medium/robot-name.js
@@ -0,0 +1,18 @@
+class Robot {
+  static usedNames = [];
+  constructor() {
+    this.name = Robot.getUniqueName();
+  }
+
+  name() {
+    return this.name;
+  }
+
+  static getUniqueName() {
+    let uniqueName = Robot.getRandomName();
+    while (Robot.usedNames.includes(uniqueName)) {
+      uniqueName = Robot.getRandomName();
+    }
+    return uniqueName;
+  }
+}
